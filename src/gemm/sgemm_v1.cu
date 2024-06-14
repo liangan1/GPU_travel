db5254e6844a7deaa805da6ef40d7c3890d2335a
Zhang,Liangang <liangang.zhang@intel.com>
2024-06-13 22:40:16 -0700

    Add the performance test

--- src/gemm/sgemm_v1.cu
+++ src/gemm/sgemm_v1.cu
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <float.h>
 
 #define OFFSET(row, col, ld) ((row) * (ld) + (col))
 #define FLOAT4(pointer) (reinterpret_cast<float4 *>(pointer)[0])
@@ -161,6 +164,75 @@ template <typename dtype> void init_matrix(dtype *mat, int size) {
   }
 }
 
+float testPerformance(
+    void (*gpuSgemm) (float *, float *, float *, const int, const int, const int),
+    dim3 gridDim, dim3 blockDim, const int M, const int N, const int K, const int repeat) {
+
+    size_t size_a = M * K * sizeof(float);
+    size_t size_b = K * N * sizeof(float);
+    size_t size_c = M * N * sizeof(float);
+
+    float *d_a, *d_b, *d_c;
+    cudaMalloc(&d_a, size_a);
+    cudaMalloc(&d_b, size_b);
+    cudaMalloc(&d_c, size_c);
+
+    cudaEvent_t start, end;
+    cudaEventCreate(&start);
+    cudaEventCreate(&end);
+    cudaEventRecord(start);
+    for (int i = 0; i < repeat; i++)
+        gpuSgemm<<<gridDim, blockDim>>>(d_a, d_b, d_c, M, N, K);
+    cudaEventRecord(end);
+    cudaEventSynchronize(end);
+
+    float msec, sec;
+    cudaEventElapsedTime(&msec, start, end);
+    sec = msec / 1000.0 / repeat;
+
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
+
+    return sec;
+}
+
+void test_performance()
+{
+    printf("\nKernal = sgemm_V1\n");
+    const int outer_repeat = 10, inner_repeat = 1;
+    const int BM = 128, BN = 128, TM = 8, TN = 8;
+    void (*gpuSgemm) (float *, float *, float *, const int, const int, const int) = sgemm_v1;
+
+    const int M_list[15] = {128, 192, 256, 384, 512, 768, 1024, 1536, 2048, 3072, 4096, 6144, 8192, 12288, 16384};
+    const int N_list[15] = {128, 192, 256, 384, 512, 768, 1024, 1536, 2048, 3072, 4096, 6144, 8192, 12288, 16384};
+    const int K_list[15] = {1024, 1024, 1024, 1024, 1024, 1024, 1024, 1024, 1024, 1024, 1024, 1024, 1024, 1024, 1024};
+    
+    const int TESTNUM = 15;
+    for (int i = 0; i < TESTNUM; i++) {
+        const int M = M_list[i], N = N_list[i], K = K_list[i];
+
+        dim3 blockDim(BN / TN, BM / TM);
+        dim3 gridDim((N + BN - 1) / BN, (M + BM - 1) / BM);
+
+        double max_sec = 0.0;
+        double min_sec = DBL_MAX;
+        double total_sec = 0.0;
+
+        for (int j = 0; j < outer_repeat; j++) {
+            double this_sec = testPerformance(gpuSgemm, gridDim, blockDim, M, N, K, inner_repeat);
+            max_sec = max(max_sec, this_sec);
+            min_sec = min(min_sec, this_sec);
+            total_sec += this_sec;
+        }
+
+        double avg_sec = total_sec / outer_repeat;
+        double avg_Gflops = ((double)M) * N * K * 2 / 1024 / 1024 / 1024 / avg_sec;
+
+        printf("M N K = %6d %6d %6d, Time = %12.8lf %12.8lf %12.8lf s, AVG Performance = %10.4lf Gflops\n", M, N, K, min_sec, avg_sec, max_sec, avg_Gflops);
+    }
+}
+
 int main() {
   float *A, *B, *C;
   int M = 512, N = 512, K = 512;
@@ -177,7 +249,8 @@ int main() {
   init_matrix(B, K * N);
   auto res = test_acc(A, B, C, M, N, K);
   if (res)
-    printf("Accuracy test pass");
+    printf("Accuracy test pass \n");
+  test_performance();
   free(A);
   free(B);
   free(C);
